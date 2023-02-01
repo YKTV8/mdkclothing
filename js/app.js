8f62400c7f906ca2b8e47af538dfa0a00dcb7c27
YKTV8 <97923330+YKTV8@users.noreply.github.com>
2023-01-31 23:27:26 -0800

    1/31 update
    changes made to css on  @media 100px - 800px. added JS for onlick event listener and toggle function.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
-const menuItems = document.getElementById("menuItems");
-const bars = document.getElementsByClassName("menu-icon").onclick = menuToggle();
+// const menuItems = document.getElementById("menuItems");
+// const bars = document.getElementsByClassName("menu-icon").onclick = menuToggle();
 
-menuItems.style.maxHeight = "0px";
+// menuItems.style.maxHeight = "0px";
 
-function menuToggle(){
-    if(menuItems.style.maxHeight == "200px"){
-        menuItems.style.maxHeight = "200px";
-    }
-    else{
-        menuItems.style.maxHeight = "0px";
+// function menuToggle(){
+//     if(menuItems.style.maxHeight == "200px"){
+//         menuItems.style.maxHeight = "200px";
+//     }
+//     else{
+//         menuItems.style.maxHeight = "0px";
+//     }
+// }
+
+// const toggleButton = document.getElementById('hamburger-menu');
+// const menu = document.getElementById('menuItems')
+
+// toggleButton.addEventListener('click', function() {
+//     menu.classList.toggle('show');
+// });
+
+function myFunction(){
+    const x = document.getElementById('menuItems');
+    if(x.style.display === "flex") {
+        x.style.display = "none";
+    }else{
+        x.style.display = "flex";
     }
 }
